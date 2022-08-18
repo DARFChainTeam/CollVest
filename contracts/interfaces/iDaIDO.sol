9d261fc1c3a0fe351c7aba8cd5addf4ab23b688c
Stan <st.takt@gmail.com>
2022-08-18 15:17:16 +0300

    interface added

---
+++ contracts/interfaces/iDaIDO.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+interface iDaIDO {
+    
+    struct Rule {
+        uint256 amountTeam;
+        uint256 amountInvest;
+        uint256 finishTime;
+    }
+
+    struct Vestors {
+        address investor;
+        uint256 amount;
+    }
+
+    struct Vesting {
+        uint256 stopDate;
+        address tokenTeam;
+        address tokenInvest;
+        uint8 voteShare;
+        bool isNative;
+        Rule[] rules;
+        
+    }
+
+    function setVesting (Rule[] calldata _rules, uint256 _amount, address _tokenTeam, address _tokenInvest, address _teamWallet) external;
+
+    function putVesting (uint256 _amount) external payable;
+
+    function claimWithdraw(uint256 _amount) external;
+
+    function claimWithdrawEth(uint256 _amount) external;
+    
+    function stopWithdraw() external;
+
+    function voteAbort(bool _vote) external;
+
+}
