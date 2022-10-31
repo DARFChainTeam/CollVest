29e4fddb4e86b130262c1c3fb0f68e71a5e25052
Stan <st.takt@gmail.com>
2022-10-31 16:27:25 +0300

    some changes in docs

--- contracts/VestFactory.sol
+++ contracts/VestFactory.sol
@@ -19,20 +19,17 @@ contract VestFactory  is i2SVstruct {
             /// @param Documents a parameter just like in doxygen (must be followed by parameter name)
             /// @return Documents the return variables of a contract’s function state variable
             /// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
-        Vesting memory vest = _vestConf;/* Vesting(
-          { amount1: _amount1, //total amount tokens of 1st side  that sends to  2nd side,  f.e. amount of team's token for this period that sends to investors.  //vested tokens, if isNative ==true, must set to "0x1"
+        Vesting memory vest = _vestConf;
+        /* Vesting(
+          { amount1: _amount1, //total amount tokens of 1st side  that sends to  2nd side,  f.e. amount of team's token for this period that sends to investors.  
           amount2:  _amount2, // total amount tokens of 2st side  that sends to  1nd side,  f.e  amount of invested  token  that sends to team            
-          token1:  _token1, //bought tokens
-          token2:  _token2, //vested tokens , if isNative ==true, must set to "0x1"
-          raisedToken1:  0,
-          raisedToken2: 0,
-/*           startDate: _startDate,
-       pausePeriod: _pausePeriod,
-          vestShare4pauseWithdraw: _vestShare4pauseWithdraw,
+          token1:  , //natibve token or address of ERC20 token contract  from vestors , if  isNative ==true, must set to "ETHCODE"
+          token2:  , //address of ERC20 token contract vested (selling) tokens , 
+           pausePeriod: 
+          vestShare4pauseWithdraw: _vestShare4pauseWithdraw, // share in sale to vestor can pause withdraw 
           voteShareAbort: _voteShareAbort, //share of stakes needed to approve voting in this vesting
           isNative:_isNative, // true if this vesting uses blockchain native token to vest, f.e. ETH in Ethereum mainnet
-          status: 0,
-          teamWallet:  _teamWallet }); */
+          teamWallet:  address of team's wallet }); */
         //if (/* typeContract == "DAIDO" */true) {
 
 
