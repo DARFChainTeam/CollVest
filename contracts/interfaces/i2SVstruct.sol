bd39f4e1d7b1f96532512e74f1005e28b1325fd4
Stan <st.takt@gmail.com>
2022-11-17 19:10:27 +0300

    added docs, softcap in struct

--- contracts/interfaces/i2SVstruct.sol
+++ contracts/interfaces/i2SVstruct.sol
@@ -18,11 +18,12 @@ interface i2SVstruct {
     struct Vesting {
         uint256 amount1; //total amount tokens of 1st side  that sends to  2nd side,  f.e. amount of team's token for this period that sends to investors
         uint256 amount2; // total amount tokens of 2st side  that sends to  1nd side,  f.e  amount of invested  token  that sends to team            
+        uint256 softCap1; //total amount tokens of 1st side  that sends to  2nd side,  f.e. amount of team's token for this period that sends to investors
+        uint256 SoftCap2; // total amount tokens of 2st side  that sends to  1nd side,  f.e  amount of invested  token  that sends to team                        
         address token1; //bought tokens
         address token2; //vested tokens,  if isNative ==true, must set to "0x1"
-
-/*         uint256 startDate; 
- */        uint256 pausePeriod; // period that withdrawing cab be pauseped until voting;
+        uint256 startDate; 
+        uint256 pausePeriod; // period that withdrawing cab be pauseped until voting;
         uint8 vestShare4pauseWithdraw; //share in percents of raised,  needed to be vested to  pause (to avoid greenmailer' dust attacks)
         uint8 voteShareAbort; //share in percents of stakes needed to approve voting in this vesting
         bool isNative; // true if this vesting uses blockchain native token to vest, f.e. ETH in Ethereum mainnet
